5f0cd7ffa7ca70cd94f3df49306487aa7db16f62
Zi Heng Xu <zihengxu139@gmail.com>
2020-04-08 04:23:26 -0400

    Updated to include date and ip

--- app/accounts/models/search_terms.py
+++ app/accounts/models/search_terms.py
@@ -1,4 +1,7 @@
 from . import *
+import 
+from datetime import datetime
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from app import db 
 from sqlalchemy.dialects.postgresql import JSON
 
@@ -6,12 +9,18 @@ class Search_terms(db.Model):
 	__tablename__ = 'search_terms'
 
 	id = db.Column(db.Integer, primary_key=True)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	user_ip = db.Column(db.String())
 	user_query = db.Column(db.String())
 	terms_query = db.Column(db.String())
+	combined_query = db.Column(db.String())
 
 	def __init__(self, user_query, terms_query):
+		self.user_ip = user_ip
+		self.date_posted = date_posted
 		self.user_query = user_query
 		self.terms_query = terms_query
+		self.combined_query = combined_query
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
@@ -19,3 +28,6 @@ class Search_terms(db.Model):
 # class Search_terms_Schema(ModelSchema):
 #   	class Meta:
 #     	model = Search_terms
+
+# class User_searches(db.Model):
+# 	id = db.Column(db.Integer, primary_key=True)
