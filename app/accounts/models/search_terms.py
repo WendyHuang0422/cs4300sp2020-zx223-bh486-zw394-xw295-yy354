57f20ee6e39b90c9651741cf4ce235b4511cec54
Zi Heng Xu <zihengxu139@gmail.com>
2020-04-08 06:03:59 -0400

    now tracks searches by IP as well as which tweets are chosen from the results
    displays a load of debug data
    count for the no. of unique IPs that searched
    1. the tweet user
    2. the topic
    3. the tweet user + topic combination
    We can show these as cool stats on the bottom such as:
    Most searched User: Donald Trump, searched 1000 times
    Most searched Topic: Coronavirus, searched 500 times
    Most searched combination: Squidward Tentacles + Krabby Patties, searched 50 times
    
    After we get the twitter API to display the relevant Tweets, I concatenate the tweet author, the first 50 strings of the tweet, and the timestamp to create the key for which to enter into the db. When a user selects a tweet to display news to check, this number will be incremented by one. We can use this count for each tweet to move their position up. Machine learning lol
    
    See the live version of this at
    https://tw-fact-xzh-2004080134.herokuapp.com/

--- app/accounts/models/search_terms.py
+++ app/accounts/models/search_terms.py
@@ -14,15 +14,68 @@ class Search_terms(db.Model):
 	terms_query = db.Column(db.String())
 	combined_query = db.Column(db.String())
 
-	def __init__(self, user_query, terms_query):
+	def __init__(self, user_ip, user_query, terms_query, combined_query):
 		self.user_ip = user_ip
-		self.date_posted = date_posted
 		self.user_query = user_query
 		self.terms_query = terms_query
 		self.combined_query = combined_query
 
 	def __repr__(self):
 		return '<id {}>'.format(self.id)
+
+class Search_users(db.Model):
+	__tablename__ = 'search_users'
+
+	id = db.Column(db.Integer, primary_key=True)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	user_ip = db.Column(db.String())
+	user_query = db.Column(db.String())
+
+	def __init__(self, user_ip, user_query):
+		self.user_ip = user_ip
+		self.user_query = user_query
+
+	def __repr__(self):
+		return '<id {}>'.format(self.id)
+
+class Tweet_checks(db.Model):
+	__tablename__ = 'tweet_checks'
+
+	id = db.Column(db.Integer, primary_key=True)
+	date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+	tweet_author = db.Column(db.String())
+	tweet_str = db.Column(db.String())
+	tweet_time = db.Column(db.String())
+	combined_str = db.Column(db.String())
+	user_ip = db.Column(db.String())
+
+	def __init__(self, user_ip, user_query):
+		self.user_ip = user_ip
+		self.tweet_author = tweet_author
+		self.tweet_str = tweet_str
+		self.tweet_time = tweet_time
+
+	def __repr__(self):
+		return '<id {}>'.format(self.id)
+
+# class Search_topic(db.Model)
+# 	__tablename__ = 'search_topic'
+
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+# 	user_ip = db.Column(db.String())
+# 	user_query = db.Column(db.String())
+# 	terms_query = db.Column(db.String())
+# 	combined_query = db.Column(db.String())
+
+# 	def __init__(self, user_ip, user_query, terms_query, combined_query):
+# 		self.user_ip = user_ip
+# 		self.user_query = user_query
+# 		self.terms_query = terms_query
+# 		self.combined_query = combined_query
+
+# 	def __repr__(self):
+# 		return '<id {}>'.format(self.id)
 
 # class Search_terms_Schema(ModelSchema):
 #   	class Meta:
--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -5,7 +5,7 @@ from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from flask import request, jsonify
 
 from app import db
-from app.accounts.models.search_terms import Search_terms
+from app.accounts.models.search_terms import Search_terms, Search_users, Tweet_checks
 from flask_sqlalchemy import SQLAlchemy
 
 project_name = "Tweeted Facts"
@@ -19,28 +19,46 @@ def search():
 	user_ip = request.remote_addr
 	if not query:
 		data = []
+		counts = []
 		user = ''
 		topic = ""
 		count_1 = 0
 		count_2 = 0
+		count_3 = 0
 	else:
 		user = query
 		if not keywords:
 			topic = ""
+			data = ["Tweet by " + query + " " + str(i) for i in range(5)]
 		else:
 			topic = keywords
-		data = range(5)
+			data = ["Tweet by " + query + " containing \"" + topic + "\" no. " + str(i + 1) for i in range(5)]
+		counts = [(Tweet_checks.query.filter(Tweet_checks.combined_str.contains(
+			user_ip + ":" + tweet))).count() for tweet in data]
+		# for tweet in data:
+		# 	if (Tweet_checks.query.filter(Tweet_checks.combined_str.contains(tweet))).count() < 1:
+
+		# 		db.session.add(tweet_data)
+		# 		db.session.commit()
 		ip_user_query = user_ip + ":" + user
 		ip_terms_query = user_ip + ":" + topic
-		if (Search_terms.query.filter(Search_terms.user_query.contains(ip_user_query))).count() < 1:
-			search_data = Search_terms(user_query = ip_user_query, terms_query = ip_terms_query)
+		combined_query = user + "<terms=>" + topic
+		ip_combined_query = user_ip + ":" + combined_query
+		if (Search_terms.query.filter(Search_terms.combined_query.contains(ip_combined_query))).count() < 1:
+			search_data = Search_terms(user_ip = user_ip, user_query = ip_user_query, terms_query = ip_terms_query,
+				combined_query = ip_combined_query)
 			db.session.add(search_data)
 			db.session.commit()
-
-		count_1 = (Search_terms.query.filter(Search_terms.user_query.contains(query))).count()
+		if (Search_users.query.filter(Search_users.user_query.contains(ip_user_query))).count() < 1:
+			search_user_data = Search_users(user_ip = user_ip, user_query = ip_user_query)
+			db.session.add(search_user_data)
+			db.session.commit()
+		count_1 = (Search_users.query.filter(Search_users.user_query.contains(query))).count()
 		count_2 = (Search_terms.query.filter(Search_terms.terms_query.contains(keywords))).count()
-	return render_template('search.html', name=project_name, netid=net_id, user=user, data=data, 
-		topic=topic, count_1=count_1, count_2 = count_2, user_ip = user_ip)
+		count_3 = (Search_terms.query.filter(Search_terms.combined_query.contains(combined_query))).count()
+
+	return render_template('search.html', name=project_name, netid=net_id, user=user, data=data, counts=counts,
+		topic=topic, count_1=count_1, count_2 = count_2, count_3 = count_3, user_ip = user_ip)
 
 # @irsystem.route("/get_my_ip", methods=["GET"])
 # def get_my_ip():
