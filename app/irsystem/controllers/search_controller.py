0fb4c8087417d90b698a32c1d22e46c5fc8c908f
Zi Heng Xu <zihengxu139@gmail.com>
2020-04-08 00:37:54 -0400

    Added in optional topic query
    Added in optional topic query to search page. Also made a preliminary design for the front end (will be improved and subject to change, of course)

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,16 +6,19 @@ project_name = "Tweeted Facts"
 net_id = "Zenong Wang: zw394, Wendy Huang: bh486, Alicia Yang: yy354, " \
 		 "Zi Heng Xu: zx223, Iris Wang: zw295"
 
-@irsystem.route('/', methods=['GET'])
+@irsystem.route("/", methods=['GET'])
 def search():
 	query = request.args.get('search')
+	keywords = request.args.get('terms', None)
 	if not query:
 		data = []
-		output_message = ''
+		user = ''
+		topic = ""
 	else:
-		output_message = "Your search: " + query
+		user = query
+		if not keywords:
+			topic = ""
+		else:
+			topic = keywords
 		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
+	return render_template('search.html', name=project_name, netid=net_id, user=user, data=data, topic=topic)
