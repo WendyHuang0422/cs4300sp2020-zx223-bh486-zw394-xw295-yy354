e820ec32bd8ba3dd4b076f2032435da16d5d1f59
yutingyang yishuzhang <yy354@cornell.edu>
2020-04-17 02:28:58 -0400

    news.py daily update

--- news.py
+++ news.py
@@ -23,28 +23,16 @@ SEED_DATA = [
         'weeksAtOne': 16
     }
 ]
-# client = pymongo.MongoClient(uri)
 
 db = client.get_default_database()
-    
-    # First we'll add a few songs. Nothing is required to create the songs 
-    # collection; it is created automatically when we insert.
 
 songs = db['songs']
 
-    # Note that the insert method can take either an array or a single dict.
-
-songs.insert_many(SEED_DATA)
-
-    # Then we need to give Boyz II Men credit for their contribution to
-    # the hit "One Sweet Day".
+# songs.insert_many(SEED_DATA)
 
 query = {'song': 'One Sweet Day'}
 
-songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})
-
-    # Finally we run a query which returns all the hits that spent 10 or
-    # more weeks at number 1.
+# songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})
 
 cursor = songs.find({'weeksAtOne': {'$gte': 10}}).sort('decade', 1)
 
@@ -54,7 +42,7 @@ for doc in cursor:
   
     ### Since this is an example, we'll clean up after ourselves.
 
-db.drop_collection('songs')
+# db.drop_collection('songs')
 
     ### Only close the connection when your app is terminating
 
