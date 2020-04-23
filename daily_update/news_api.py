ad0253e73e507281fbb147fc71ea1c7c4ed4316b
yutingyang yishuzhang <yy354@cornell.edu>
2020-04-23 13:48:21 -0400

    heorku db

--- daily_update/news_api.py
+++ daily_update/news_api.py
@@ -101,7 +101,7 @@ def news_Aggregated(N, date1, date2, order, query=None):
     urllib
     json
     """
-    print("news_api using  N is  ",N)
+    print("news_api using  N is  ", N)
     instance = retrieve_news_article(
         N, '770d3e15e7234b028da0d84fc0fb6210', date1, date2, order, query)
     wanted = ['source', 'author', 'description',
@@ -118,6 +118,7 @@ def news_Aggregated(N, date1, date2, order, query=None):
         data1['content'] = idx['content']
         full1.append(data1)
         count += 1
+    print("count is ***************", count)
     return full1
 
 
