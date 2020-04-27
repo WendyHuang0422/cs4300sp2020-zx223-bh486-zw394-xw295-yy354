822970fcce249a8e7db30965ec63798bef799d3e
Zi Heng Xu <zihengxu139@gmail.com>
2020-04-27 08:33:58 -0400

    Added lots of front end functionality and interface improvements
    Now displays user's display name isntead of the username string.
    
    Redesigned logo
    
    Cleaned up frontend code using layouts
    
    displays a lot of additional information
    
    TODO:
    add a header and footer
    make the view_tweet page
    
    WOuld it be possible to use a display name (or part of it), e.g. "Donald Trump" and find a user with the display name closest to "Donald Trump" and return their user ID? This would allow users to search using the display name (Donald Trump) instead of their username (realDonaldTrump).

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -46,31 +46,64 @@ def search_for():
 	if topic == "": topic = None
 
 	length = 0
-	data = []
-	date = []
-	retweets = []
-	like = []
+	# contains user data: [display name, url, verified]
+	tw_user_data = []
+	# contains user x_counts: [followers, friends, listed, favourites, statuses]
+	tw_user_counts = []
+	# contains links for user media objects: [profile_banner_url, profile_image_url_https]
+	tw_user_media_obj = []
+	tw_text = []
+	tw_date = []
+	tw_retweets = []
+	tw_like = []
 	news_list = []
 	error = []
 	# padding = "_".ljust(len(user) + 2, "_")
 	start_time = time.time()
-
+	xzh_wanted_user_keys = ["followers_count", "friends_count", "listed_count", \
+	"favourites_count", "statuses_count", "profile_banner_url", "profile_image_url_https"]
 	try:
 		# result = adhoc_data_crawl.totally_aggregated(user, 3, False, topic, N_keyword = 5, \
 		# 	num_processed_tweets = 100, num_pool_tweets = 200, nltk1 = True)
 		result = adhoc_data_crawl.totally_aggregated(user, 3, False, topic, N_keyword = 5, \
 			num_processed_tweets = 20, num_pool_tweets = 40, nltk1 = True)
+
 		length = min(len(result[0]), 3)
 
 		for i in range(length):
 			tweet = result[0][i][0]
-			data.append(tweet["text"])
-			date.append(tweet["created_at"])
-			retweets.append(tweet["retweet_count"])
-			like.append(tweet["favorite_count"])
+			# print(tweet)
+			if i == 0:
+				user_tw_acc_url = "https://twitter.com/" + user
+				tweet_author = tweet["user"]
+				tw_user_data = [tweet_author["name"], user_tw_acc_url, tweet_author["verified"]]
+				tw_user_counts = [tweet_author["followers_count"], tweet_author["friends_count"], \
+				tweet_author["listed_count"], tweet_author["favourites_count"], \
+				tweet_author["statuses_count"]]
+
+				tw_user_media_obj = [tweet_author["profile_banner_url"], \
+				tweet_author["profile_image_url_https"]]
+
+			tw_text.append(tweet["text"])
+			time_stamp = tweet["created_at"].split(" ")
+			time_o_clock = time_stamp[3].split(":")
+			am_pm_time = ""
+			if int(time_o_clock[0]) == 0:
+				am_pm_time = am_pm_time + "12" + ":" + time_o_clock[1] + " AM"
+			elif int(time_o_clock[0]) < 12:
+				am_pm_time = am_pm_time + time_o_clock[0] + ":" + time_o_clock[1] + " AM"
+			else:
+				hour = int(time_o_clock[0]) - 12
+				am_pm_time = am_pm_time + str(hour) + ":" + time_o_clock[1] + " PM"
+
+			time_stamp_string = time_stamp[1] + " " + time_stamp[2] + " " + am_pm_time
+			tw_date.append(time_stamp_string)
+			tw_retweets.append(tweet["retweet_count"])
+			tw_like.append(tweet["favorite_count"])
 			tweet_news = []
 
 			for news in result[1][i]:
+
 				source = news[0]["source"]
 				if source in leaning.keys():
 					source_leaning = leaning_ref[leaning[source]]
@@ -99,7 +132,8 @@ def search_for():
 	
 	return render_template("results.html", \
 		user = user, topic = topic, \
-		data = data, length = length, date = date, retweets = retweets, like = like, \
+		tw_text = tw_text, length = length, tw_date = tw_date, tw_retweets = tw_retweets, tw_like = tw_like, \
+		tw_user_data = tw_user_data, tw_user_counts = tw_user_counts, tw_user_media_obj = tw_user_media_obj, \
 		news_list = news_list, \
 		error = error, time_taken = time_taken)
 
